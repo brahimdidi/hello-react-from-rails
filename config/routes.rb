18ec3d13fddbceb3d42097ca0d26931cdeb81016
brahimdidi <brahimgb1team@gmail.com>
2022-10-04 16:41:24 +0000

    set static as the root of the  app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'static/index'
   get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'root#index'
+  root 'static#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
