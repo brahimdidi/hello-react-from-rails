5e857b3c54d03d38135f05a0e70373acc412d741
brahimdidi <brahimgb1team@gmail.com>
2022-10-04 17:00:40 +0000

    use module for the api

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,9 @@
-class API::MessagesController < ApplicationController
-  def index
-    Message.order(Arel.sql('RANDOM()')).first.greeting
+module Api
+  class API::MessagesController < ApplicationController
+    def index
+      Message.order(Arel.sql('RANDOM()')).first.greeting
+    end
   end
 end
+
+
